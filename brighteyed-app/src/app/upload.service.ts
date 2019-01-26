f6cfa798656edb36ece516171755583a203af264
Moritz Meister <meister.mo@gmail.com>
2019-01-26 17:06:37 +0100

    Modify Patient View

--- brighteyed-app/src/app/upload.service.ts
+++ brighteyed-app/src/app/upload.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpParams, HttpRequest, HttpEvent, HttpHeaders} from '@angular/common/http';
-import {Observable} from "rxjs";
+import { HttpClient, HttpParams, HttpRequest, HttpEvent, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { Http, Response} from '@angular/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 
 export class UploadService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   // file from event.target.files[0]
   uploadFile(url: string, file: File, name: string): Observable<HttpEvent<any>> {
@@ -34,5 +38,35 @@ export class UploadService {
     const req = new HttpRequest('POST', url, formData, options);
     console.log(req);
     return this.http.request(req);
+  }
+
+  public getWallet(): Observable<any> {
+    console.log('Get wallet');
+
+    let params = new HttpParams();
+    let headers = new HttpHeaders();
+
+    headers.set('Accept', 'application/json');
+
+    const options = {
+      withCredentials: true,
+      headers: headers,
+    };
+
+    const req = new HttpRequest('GET', 'http://localhost:3000/api/wallet', options);
+    console.log(req);
+    return this.http.request(req);
+  }
+
+  private extractData(res: Response): any {
+    return res.json();
+  }
+  private handleError(error: any): Observable<string> {
+    // In a real world app, we might use a remote logging infrastructure
+    // We'd also dig deeper into the error to get a better message
+    const errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errMsg); // log to console instead
+    return Observable.throw(errMsg);
   }
 }
