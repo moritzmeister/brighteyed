af973f453be96ad1f4698688e28eec22c9b69974
Serghei Bordea <bordeaserghei@gmail.com>
2019-01-27 15:30:36 +0100

    more scripts for the network

---
+++ brighteyed-network/createParticipants.sh
@@ -0,0 +1,4 @@
+./issueId.sh Doctor 1111 Moritz Meister moritz@gmail.com
+./issueId.sh Doctor 2222 Jioele Begini jioele@gmail.com
+./issueId.sh Patient 3333 Serghei Bordea serghei@gmail.com
+./issueId.sh Patient 4444 Nico Righeriu nico@gmail.com
