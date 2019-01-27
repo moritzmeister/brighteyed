8e9b9572b31c2c29ba0e07b0e0699abfb5d5ae96
Serghei Bordea <bordeaserghei@gmail.com>
2019-01-27 16:07:56 +0100

    EasyRun.md updated

--- brighteyed-network/createParticipants.sh
+++ brighteyed-network/createParticipants.sh
@@ -2,3 +2,9 @@
 ./issueId.sh Doctor 2222 Jioele Begini jioele@gmail.com
 ./issueId.sh Patient 3333 Serghei Bordea serghei@gmail.com
 ./issueId.sh Patient 4444 Nico Righeriu nico@gmail.com
+./createPatient.sh Doctor 5555 Jessica Parker jessika@gmail.com
+./createPatient.sh Doctor 6666 Cameron Diaz cameron@gmail.com
+./createPatient.sh Doctor 7777 Julia Roberts julia@gmail.com
+./createPatient.sh Patient 8888 Vasilii Bodarev vasilii@gmail.com
+./createPatient.sh Patient 9999 Anastasia Volochkova anastasia@gmail.com
+./createPatient.sh Patient 0000 Wladimir Klitschko wladimir@gmail.com
--- brighteyed-network/issueId.sh
+++ brighteyed-network/issueId.sh
@@ -1,8 +1,2 @@
 ./createParticipant.sh $1 $2 $3 $4 $5
-if [ $1 == "Doctor" ]; then
-    composer identity issue -c admin@brighteyed-network -f $3.card -u $3 -a "resource":"org.brighteyed.network.Doctor#$2"
-elif [ $1 == "Patient" ]; then
-    composer identity issue -c admin@brighteyed-network -f $3.card -u $3 -a "resource":"org.brighteyed.network.Patient#$2"
-else
-    echo "Unknown person type"
-fi
+composer identity issue -c admin@brighteyed-network -f $3.card -u $3 -a "resource":"org.brighteyed.network.$1#$2"
